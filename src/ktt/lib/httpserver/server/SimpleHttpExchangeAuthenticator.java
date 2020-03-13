af31fea887f2399e980b1010de037e01bcfc048c
Katsute <Katsute@users.noreply.github.com>
2020-03-13 09:52:37 -0400

    Update SimpleHttpExchangeAuthenticator.java

--- src/ktt/lib/httpserver/server/SimpleHttpExchangeAuthenticator.java
+++ src/ktt/lib/httpserver/server/SimpleHttpExchangeAuthenticator.java
@@ -1,7 +1,5 @@
 package ktt.lib.httpserver.server;
 
-import ktt.lib.httpserver.server.SimpleHttpExchange;
-
 public interface SimpleHttpExchangeAuthenticator {
 
     default boolean authenticate(final SimpleHttpExchange exchange){ return true; }
