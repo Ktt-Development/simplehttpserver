f47f90de3afd0bb7e6c0378f15a2e104cb397e4f
Katsute <Katsute@users.noreply.github.com>
2020-03-12 23:01:49 -0400

    Create FileHandler.java

---
+++ src/ktt/lib/httpserver/handler/FileHandler.java
@@ -0,0 +1,15 @@
+package ktt.lib.httpserver.handler;
+
+import ktt.lib.httpserver.server.SimpleHttpExchange;
+import ktt.lib.httpserver.server.SimpleHttpHandler;
+
+import java.io.IOException;
+
+public class FileHandler extends SimpleHttpHandler {
+
+    @Override
+    public void handle(final SimpleHttpExchange exchange) throws IOException{
+        // todo
+    }
+
+}
