0a02094208a35e3140434324b0f36fe508c7d2c0
Katsute <Katsute@users.noreply.github.com>
2020-05-05 11:47:43 -0400

    Update TemporaryHandler.java

--- src/main/java/com/kttdevelopment/simplehttpserver/handler/TemporaryHandler.java
+++ src/main/java/com/kttdevelopment/simplehttpserver/handler/TemporaryHandler.java
@@ -9,17 +9,26 @@ public class TemporaryHandler implements HttpHandler {
 
     private final HttpHandler handler;
 
+    private boolean hasExpiry;
+    private long initTime;
+    private long maxTime;
+
     public TemporaryHandler(final HttpHandler handler){
         this.handler = handler;
     }
 
     public TemporaryHandler(final HttpHandler handler, final long maxTime){
         this.handler = handler;
+
+        hasExpiry = true;
+        initTime = System.currentTimeMillis();
+        this.maxTime = maxTime;
     }
 
     @Override
     public final void handle(final HttpExchange exchange) throws IOException{
-        handler.handle(exchange);
+        if(!hasExpiry || maxTime + initTime < System.currentTimeMillis())
+            handler.handle(exchange);
         exchange.getHttpContext().getServer().removeContext(exchange.getHttpContext());
     }
 
