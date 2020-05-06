d629ac34f723b3bde70f5e314293671f5ea30557
Katsute <Katsute@users.noreply.github.com>
2020-05-05 21:23:08 -0400

    More documentation

--- src/main/java/com/kttdevelopment/simplehttpserver/handler/ServerThrottler.java
+++ src/main/java/com/kttdevelopment/simplehttpserver/handler/ServerThrottler.java
@@ -21,28 +21,77 @@ public class ServerThrottler extends ConnectionThrottler {
 
     private final AtomicInteger maxConnections = new AtomicInteger(0);
 
+    /**
+     * Creates a throttler that allows no connections.
+     *
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public ServerThrottler(){
         countsTowardsLimit = exchange -> true;
     }
 
+    /**
+     * Creates a throttler that allows a certain amount of simultaneous connections.
+     *
+     * @param maxConnections maximum simultaneous connections
+     *
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public ServerThrottler(final int maxConnections){
         countsTowardsLimit = exchange -> true;
         this.maxConnections.set(maxConnections);
     }
 
+    /**
+     * Creates a throttler that allows a certain amount of simultaneous connections and exempt connections.
+     *
+     * @param countsTowardsLimit determines which exchanges count towards the connection limit
+     *
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public ServerThrottler(final Predicate<HttpExchange> countsTowardsLimit){
         this.countsTowardsLimit = countsTowardsLimit;
     }
 
+    /**
+     * Creates a throttler that allows a certain amount of simultaneous connections and exempt connections.
+     *
+     * @param countsTowardsLimit determines which exchanges count towards the connection limit
+     * @param maxConnections maximum simultaneous connections
+     *
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public ServerThrottler(final Predicate<HttpExchange> countsTowardsLimit, final int maxConnections){
         this.countsTowardsLimit = countsTowardsLimit;
         this.maxConnections.set(maxConnections);
     }
 
+    /**
+     * Returns the maximum allowed connections.
+     *
+     * @return maximum allowed connections
+     *
+     * @see #setMaxConnections(int)
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public final int getMaxConnections(){
         return maxConnections.get();
     }
 
+    /**
+     * Sets the maximum allowed connections. Must be a positive number.
+     *
+     * @param maxConnections maximum allowed connections
+     *
+     * @see #getMaxConnections()
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public synchronized final void setMaxConnections(final int maxConnections){
         if(maxConnections >= 0)
             this.maxConnections.set(maxConnections);
--- src/main/java/com/kttdevelopment/simplehttpserver/handler/SessionThrottler.java
+++ src/main/java/com/kttdevelopment/simplehttpserver/handler/SessionThrottler.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 
 /**
- * Limits connections per http session.
+ * Limits connections per http session. This can be used to limit simultaneous downloads.
  *
  * @see ThrottledHandler
  * @see ServerThrottler
@@ -26,32 +26,86 @@ public class SessionThrottler extends ConnectionThrottler {
 
     private final AtomicInteger maxConnections = new AtomicInteger(0);
 
+    /**
+     * Creates a throttler that allows no connections.
+     *
+     * @param sessionHandler session handler
+     *
+     * @see HttpSessionHandler
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public SessionThrottler(final HttpSessionHandler sessionHandler){
         this.sessionHandler = sessionHandler;
         countsTowardsLimit = (exchange) -> true;
     }
 
+    /**
+     * Creates a throttler that limits the amount of simultaneous connections a user can have.
+     *
+     * @param sessionHandler session handler
+     * @param maxConnections maximum simultaneous connections (per user)
+     *
+     * @see HttpSessionHandler
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public SessionThrottler(final HttpSessionHandler sessionHandler, final int maxConnections){
         this.sessionHandler = sessionHandler;
         countsTowardsLimit = (exchange) -> true;
         this.maxConnections.set(maxConnections);
     }
 
+    /**
+     * Creates a throttler that limits the amount of simultaneous connections a user can have and exempt connections.
+     *
+     * @param sessionHandler session handler
+     * @param countsTowardsLimit determines which users are subject to the limit
+     *
+     * @see HttpSessionHandler
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public SessionThrottler(final HttpSessionHandler sessionHandler, final Predicate<HttpSession> countsTowardsLimit){
         this.sessionHandler = sessionHandler;
         this.countsTowardsLimit = countsTowardsLimit;
     }
 
+    /**
+     * Creates a throttler that limits the amount of simultaneous connections a user can have and exempt connections.
+     *
+     * @param sessionHandler session handler
+     * @param countsTowardsLimit determines which users are subject to the limit
+     * @param maxConnections maximum simultaneous connections (per user)
+     */
     public SessionThrottler(final HttpSessionHandler sessionHandler, final Predicate<HttpSession> countsTowardsLimit, final int maxConnections){
         this.sessionHandler = sessionHandler;
         this.countsTowardsLimit = countsTowardsLimit;
         this.maxConnections.set(maxConnections);
     }
 
+    /**
+     * Returns the maximum allowed connections.
+     *
+     * @return maximum allowed connections (per user)
+     *
+     * @see #setMaxConnections(int)
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public final int getMaxConnections(){
         return maxConnections.get();
     }
 
+    /**
+     * Sets the maximum allowed connections. Must be a positive number.
+     *
+     * @param maxConnections maximum allowed connections (per user)
+     *
+     * @see #getMaxConnections()
+     * @since 03.03.00
+     * @author Ktt Development
+     */
     public synchronized final void setMaxConnections(final int maxConnections){
         if(maxConnections >= 0)
             this.maxConnections.set(maxConnections);
