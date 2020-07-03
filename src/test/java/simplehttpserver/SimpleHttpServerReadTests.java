8aa40d8ce5507438fb8f51ea424e411025027c65
Katsute <Katsute@users.noreply.github.com>
2020-07-03 12:49:30 -0400

    fix ports

--- src/test/java/simplehttpserver/SimpleHttpServerReadTests.java
+++ src/test/java/simplehttpserver/SimpleHttpServerReadTests.java
@@ -12,9 +12,13 @@ public class SimpleHttpServerReadTests {
 
     @Test
     public void get() throws IOException{
-        final SimpleHttpServer server = SimpleHttpServer.create(port);
+        final SimpleHttpServer server = SimpleHttpServer.create();
         Assert.assertNotNull("#getHttpServer() should not be null", server.getHttpServer());
-        Assert.assertTrue("#getAddress() should noy only be not null, it should also be the local address of the server running it",server.getAddress().getAddress().isAnyLocalAddress());
+
+        Assert.assertNull("#getAddress() should be null for unbinded server",server.getAddress());
+        server.bind(port);
+        Assert.assertTrue("#getAddress() should be the local address of the server running it",server.getAddress().getAddress().isAnyLocalAddress());
+
         Assert.assertNull("#getExecutor() should initially be null",server.getExecutor());
         Assert.assertNull("#getHttpSessionHandler() should initially be null",server.getHttpSessionHandler());
         Assert.assertTrue("#getContexts() should initially be empty on server instantiation",server.getContexts().isEmpty());
