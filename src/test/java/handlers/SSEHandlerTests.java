bea0e0e17d5e582722112cb310d59b18f83d01ff
Katsute <Katsute@users.noreply.github.com>
2020-07-03 17:54:23 -0400

    Update SSEHandlerTests.java

--- src/test/java/handlers/SSEHandlerTests.java
+++ src/test/java/handlers/SSEHandlerTests.java
@@ -1,13 +1,29 @@
 package handlers;
 
+import com.kttdevelopment.simplehttpserver.SimpleHttpServer;
+import com.kttdevelopment.simplehttpserver.handler.SSEHandler;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class SSEHandlerTests {
 
     @Test @Ignore
-    public void test(){ // push & receive
+    public void test() throws IOException{
+        final int port = 30006;
 
+        final SimpleHttpServer server = SimpleHttpServer.create(port);
+        final URL target = new URL("http://localhost:" + port);
+
+        final SSEHandler handler = new SSEHandler();
+        server.createContext("",handler);
+        server.start();
+
+
+
+        server.stop();
     }
 
 }
