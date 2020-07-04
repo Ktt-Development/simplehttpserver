17d0a5ac2372e005f1424f8773797c83efa31fc6
Katsute <Katsute@users.noreply.github.com>
2020-07-04 17:23:20 -0400

    Add walking tests

--- src/test/java/handlers/FileHandlerTests.java
+++ src/test/java/handlers/FileHandlerTests.java
@@ -1,8 +1,7 @@
 package handlers;
 
 import com.kttdevelopment.simplehttpserver.*;
-import com.kttdevelopment.simplehttpserver.handler.FileHandler;
-import com.kttdevelopment.simplehttpserver.handler.FileHandlerAdapter;
+import com.kttdevelopment.simplehttpserver.handler.*;
 import org.junit.*;
 
 import java.io.File;
@@ -14,7 +13,7 @@ import java.util.concurrent.ExecutionException;
 public class FileHandlerTests {
 
     @Test @Ignore
-    public void addFileTests() throws IOException{
+    public void addFileTests() throws IOException{ // test load types
         final int port = 25001;
         final SimpleHttpsServer server = SimpleHttpsServer.create(port);
         final FileHandler handler = new FileHandler();
@@ -79,9 +78,146 @@ public class FileHandlerTests {
 
     }
 
-    @Test @Ignore
-    public void addDirectoryTests(){
+    @Test
+    public void addDirectoryTestsNoWalk() throws IOException, InterruptedException{
         final int port = 25003;
+        final SimpleHttpServer server = SimpleHttpServer.create(port);
+        final FileHandler handler = new FileHandler();
+        final String contextNoName = "alt";
+        final String contextWName = "altn";
+        final String dirNewName = "dirName";
+
+        final String testRealFile = "test.txt", testFileContent = "readme";
+        final String noReadDir = "dirnoread";
+        final String noReadFile = "innerNoRead.txt";
+
+        final File noWalk = new File("src/test/resources/directory/nowalk");
+
+        final String context = "";
+
+        handler.addDirectory(noWalk); // test file & directory read
+        handler.addDirectory(contextNoName,noWalk);
+        handler.addDirectory(noWalk,dirNewName);
+        handler.addDirectory(contextWName,noWalk,dirNewName);
+
+        server.createContext(context,handler);
+        server.start();
+
+        final String[] validPathsToTest = { // valid reads
+            noWalk.getName() + '/' + testRealFile,
+            contextNoName + '/' + noWalk.getName()  + '/' + testRealFile,
+            dirNewName + '/' + testRealFile,
+            contextWName + '/' + dirNewName + '/' + testRealFile
+        };
+
+        for(final String path : validPathsToTest){
+            String url = "http://localhost:" + port + '/' + path;
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+            try{
+                String response = HttpClient.newHttpClient().sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body).get();
+
+                Assert.assertNotNull("Client did not find data for " + path,response);
+                Assert.assertEquals("Client data did not match server data for " + path,testFileContent,response);
+            }catch(ExecutionException e){
+                Assert.fail("Client did not find data for " + path);
+            }
+        }
+
+        final String[] invalidPathsToTest = {
+            noWalk.getName() + '/' + noReadDir,
+            noWalk.getName() + '/' + noReadDir + '/' + noReadFile
+        };
+
+        for(final String path : invalidPathsToTest){
+            String url = "http://localhost:" + port + '/' +  path;
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+            Exception exception = null;
+            try{
+                String response = HttpClient.newHttpClient().sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body).get();
+
+                Assert.assertNull("Client found data for blocked path " + path,response);
+            }catch(ExecutionException e){
+                exception = e;
+            }
+            Assert.assertNotNull("Client found data for blocked path",exception);
+        }
+
+        server.stop();
+    }
+
+    @Test @Ignore
+    public void addDirectoryTestsWalk() throws IOException, InterruptedException{
+        final int port = 25004;
+        final SimpleHttpServer server = SimpleHttpServer.create(port);
+        final FileHandler handler = new FileHandler();
+
+        final String testRealFile = "test.txt", testFileContent = "readme";
+        final String readDir = "dirnoread";
+        final String innerFile = "innerNoRead.txt";
+
+        final File noWalk = new File("src/test/resources/directory/nowalk");
+
+        final String context = "";
+
+        // handler.addDirectory(noWalk,true);
+        handler.addDirectory(noWalk, ByteLoadingOption.LIVELOAD,true); // todo: def liveload for walk param
+
+        server.createContext(context,handler);
+        server.start();
+
+        final String[] validPathsToTest = { // valid reads
+            noWalk.getName() + '/' + testRealFile,
+            noWalk.getName() + '/' + readDir + '/' + innerFile
+        };
+
+        for(final String path : validPathsToTest){
+            String url = "http://localhost:" + port + '/' + path;
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+            try{
+                String response = HttpClient.newHttpClient().sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body).get();
+
+                Assert.assertNotNull("Client did not find data for " + path,response);
+                Assert.assertEquals("Client data did not match server data for " + path,testFileContent,response);
+            }catch(ExecutionException e){
+                Assert.fail("Client did not find data for " + path);
+            }
+        }
+
+        final String[] invalidPathsToTest = {
+            noWalk.getName() + '/' + readDir
+        };
+
+        for(final String path : invalidPathsToTest){
+            String url = "http://localhost:" + port + '/' +  path;
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+            Exception exception = null;
+            try{
+                String response = HttpClient.newHttpClient().sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body).get();
+
+                Assert.assertNull("Client found data for blocked path " + path,response);
+            }catch(ExecutionException e){
+                exception = e;
+            }
+            Assert.assertNotNull("Client found data for blocked path",exception);
+        }
+
+        server.stop();
     }
 
 }
