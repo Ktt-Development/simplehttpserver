1dbc43230d6f7cfca6d79699d7d2892c88732851
Katsute <Katsute@users.noreply.github.com>
2020-07-04 12:54:52 -0400

    SSE tests

--- src/test/java/handlers/SSEHandlerTests.java
+++ src/test/java/handlers/SSEHandlerTests.java
@@ -4,17 +4,21 @@ import com.kttdevelopment.simplehttpserver.SimpleHttpServer;
 import com.kttdevelopment.simplehttpserver.handler.SSEHandler;
 import org.junit.*;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.*;
+import java.time.Duration;
+import java.util.concurrent.*;
 
 public class SSEHandlerTests {
 
-    @Test @Ignore
-    public void test() throws IOException{
+    @SuppressWarnings({"unchecked", "Convert2Lambda", "rawtypes"})
+    @Test
+    public void test() throws IOException, ExecutionException, InterruptedException{
         final int port = 30006;
 
         final SimpleHttpServer server = SimpleHttpServer.create(port);
-        final URL target = new URL("http://localhost:" + port);
 
         final SSEHandler handler = new SSEHandler();
         server.createContext("",handler);
@@ -22,12 +26,43 @@ public class SSEHandlerTests {
 
         // todo: connect to sse
 
+        HttpRequest request = HttpRequest.newBuilder()
+             .uri(URI.create("http://localhost:" + port))
+             .timeout(Duration.ofSeconds(2))
+             .build();
+
+        final HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<InputStream> response = client.sendAsync(request, HttpResponse.BodyHandlers.ofInputStream()).get();
+
         final String result = "result";
         handler.push(result);
 
-        String response = null;
+        BufferedReader IN = new BufferedReader(new InputStreamReader(response.body()));
+        StringBuilder OUT = new StringBuilder();
 
-        Assert.assertEquals("Client did not match event pushed by server",result,response);
+        final Duration dur = Duration.ofSeconds(2);
+        final ExecutorService executor = Executors.newSingleThreadExecutor();
+        final Future<String> future = executor.submit(new Callable() {
+            @Override
+            public String call(){
+                String ln;
+                try{
+                    while((ln = IN.readLine()) != null)
+                            OUT.append(ln).append('\n');
+                }catch(IOException e){
+                    Assert.fail("Unable to read input stream from client");
+                }
+                return OUT.toString();
+            }
+        });
+
+        try {
+            future.get(dur.toMillis(), TimeUnit.MILLISECONDS);
+        } catch (TimeoutException e) {
+            future.cancel(true);
+        }
+
+        Assert.assertTrue("Client event stream did not match server event",OUT.toString().contains("id: 0\ndata: " + result));
 
         server.stop();
     }
