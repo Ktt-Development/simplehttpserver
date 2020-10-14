24584cc1b2c8ae5fd0dbe0f044b1f57ecb27e06f
Katsute <58778985+Katsute@users.noreply.github.com>
2020-10-14 17:07:16 -0400

    replace core with junit

--- src/test/java/com/kttdevelopment/simplehttpserver/handlers/file/FileHandlerWalkTest.java
+++ src/test/java/com/kttdevelopment/simplehttpserver/handlers/file/FileHandlerWalkTest.java
@@ -3,17 +3,21 @@ package com.kttdevelopment.simplehttpserver.handlers.file;
 import com.kttdevelopment.core.tests.TestUtil;
 import com.kttdevelopment.simplehttpserver.SimpleHttpServer;
 import com.kttdevelopment.simplehttpserver.handler.FileHandler;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.*;
+import java.nio.file.Files;
 import java.util.concurrent.ExecutionException;
 
 @SuppressWarnings("SpellCheckingInspection")
 public final class FileHandlerWalkTest {
+
+    @Rule
+    public final TemporaryFolder directory = new TemporaryFolder(new File("src/test/resources"));
 
     @Test
     public final void addDirectoryTestsWalk() throws IOException, InterruptedException{
@@ -21,25 +25,27 @@ public final class FileHandlerWalkTest {
         final SimpleHttpServer server = SimpleHttpServer.create(port);
         final FileHandler handler     = new FileHandler();
 
-        final String testRealFile   = "test.txt", testFileContent = "readme";
-        final String readDir        = "dirnoread";
-        final String innerFile      = "innerNoRead.txt";
+        final String fileName       = "file";
+        final String testContent    = String.valueOf(System.currentTimeMillis());
 
-        final File noWalk = new File("src/test/resources/directory/nowalk");
+        final File dir      = directory.getRoot();
+        final File subdir   = directory.newFolder();
 
-        TestUtil.createTestFile(new File(noWalk, testRealFile), testFileContent);
-        TestUtil.createTestFile(new File(new File(noWalk,readDir),innerFile),testFileContent);
+        final File file = new File(directory.getRoot(),fileName);
+        Files.write(file.toPath(), testContent.getBytes());
+        final File walk = new File(subdir,fileName);
+        Files.write(walk.toPath(),testContent.getBytes());
 
         final String context = "";
 
-        handler.addDirectory(noWalk,true);
+        handler.addDirectory(dir,true);
 
         server.createContext(context,handler);
         server.start();
 
         final String[] validPathsToTest = { // valid reads
-            noWalk.getName() + '/' + testRealFile,
-            noWalk.getName() + '/' + readDir + '/' + innerFile
+            dir.getName() + '/' + file.getName(),
+            dir.getName() + '/' + subdir.getName() + '/' + walk.getName()
         };
 
         for(final String path : validPathsToTest){
@@ -49,18 +55,20 @@ public final class FileHandlerWalkTest {
                 .build();
 
             try{
-                final String response = HttpClient.newHttpClient().sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                    .thenApply(HttpResponse::body).get();
+                final String response = HttpClient.newHttpClient()
+                    .sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .get();
 
                 Assert.assertNotNull("Client did not find data for " + path,response);
-                Assert.assertEquals("Client data did not match server data for " + path,testFileContent,response);
+                Assert.assertEquals("Client data did not match server data for " + path,testContent,response);
             }catch(final ExecutionException ignored){
                 Assert.fail("Client did not find data for " + path);
             }
         }
 
         final String[] invalidPathsToTest = {
-            noWalk.getName() + '/' + readDir
+            dir.getName() + '/' + subdir.getName()
         };
 
         for(final String path : invalidPathsToTest){
@@ -71,8 +79,10 @@ public final class FileHandlerWalkTest {
 
             Exception exception = null;
             try{
-                String response = HttpClient.newHttpClient().sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                    .thenApply(HttpResponse::body).get();
+                final String response = HttpClient.newHttpClient()
+                    .sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .get();
 
                 Assert.assertNull("Client found data for blocked path " + path,response);
             }catch(final ExecutionException e){
