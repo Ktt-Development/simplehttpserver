b3a5942d54a793eb456510c8fdc400b03ef78496
Katsute <58778985+Katsute@users.noreply.github.com>
2020-10-19 11:17:02 -0400

    Add additional check

--- src/test/java/com/kttdevelopment/simplehttpserver/handlers/file/FileHandlerCacheTest.java
+++ src/test/java/com/kttdevelopment/simplehttpserver/handlers/file/FileHandlerCacheTest.java
@@ -36,18 +36,28 @@ public class FileHandlerCacheTest {
 
         final FileHandler handler = new FileHandler(new CacheFileAdapter(cacheTime));
         final File file = new File(dir, UUID.randomUUID().toString());
+        final String filename = file.getName();
         Files.write(file.toPath(), content.getBytes());
+
         final File file2 = new File(dir, UUID.randomUUID().toString());
-        Assertions.assertTrue(file2.createNewFile());
+        final String content2 = UUID.randomUUID().toString();
+        Files.write(file2.toPath(), content2.getBytes());
+
+        final File file3 = new File(dir, UUID.randomUUID().toString());
+        Assertions.assertTrue(file3.createNewFile());
         handler.addFile(file, ByteLoadingOption.CACHELOAD);
-        handler.addFile(file2);
+        handler.addFile(file3);
 
         server.createContext("", handler);
         server.start();
 
+        final String check = "bytes=" + Arrays.toString(content.getBytes());
+        final String check2 = "bytes=" + Arrays.toString(content2.getBytes());
         // access new file
+        Assertions.assertFalse(handler.toString().contains(check));
+        Assertions.assertFalse(handler.toString().contains(check2));
         try{
-            final String url = "http://localhost:" + port + '/' + file.getName();
+            final String url = "http://localhost:" + port + '/' + filename;
             final HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
@@ -57,17 +67,18 @@ public class FileHandlerCacheTest {
                 .thenApply(HttpResponse::body)
                 .get();
 
-            Assertions.assertNotNull(response, "Client did not find data for " + file.getName());
-            Assertions.assertEquals(content, response, "Client data did not match server data for " + file.getName());
+            Assertions.assertNotNull(response, "Client did not find data for " + filename);
+            Assertions.assertEquals(content, response, "Client data did not match server data for " + filename);
         }catch(final ExecutionException ignored){
-            Assertions.fail("Client did not find data for " + file.getName());
+            Assertions.fail("Client did not find data for " + filename);
         }
-        Assertions.assertTrue(handler.toString().contains("bytes=" + Arrays.toString(content.getBytes())));
+        Assertions.assertTrue(handler.toString().contains(check));
+        Assertions.assertFalse(handler.toString().contains(check2));
 
         // check if file is cleared after expiry
         Thread.sleep(cacheTime + 1000);
         {
-            final String url = "http://localhost:" + port + '/' + file2.getName();
+            final String url = "http://localhost:" + port + '/' + file3.getName();
             final HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
@@ -77,11 +88,12 @@ public class FileHandlerCacheTest {
                 .thenApply(HttpResponse::body)
                 .get();
 
-            Assertions.assertFalse(handler.toString().contains("bytes=" + Arrays.toString(content.getBytes())));
+            Assertions.assertFalse(handler.toString().contains(check));
+            Assertions.assertFalse(handler.toString().contains(check2));
         }
         // access file again
         try{
-            final String url = "http://localhost:" + port + '/' + file.getName();
+            final String url = "http://localhost:" + port + '/' + filename;
             final HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
@@ -91,12 +103,13 @@ public class FileHandlerCacheTest {
                 .thenApply(HttpResponse::body)
                 .get();
 
-            Assertions.assertNotNull(response, "Client did not find data for " + file.getName());
-            Assertions.assertEquals(content, response, "Client data did not match server data for " + file.getName());
+            Assertions.assertNotNull(response, "Client did not find data for " + filename);
+            Assertions.assertEquals(content, response, "Client data did not match server data for " + filename);
         }catch(final ExecutionException ignored){
-            Assertions.fail("Client did not find data for " + file.getName());
+            Assertions.fail("Client did not find data for " + filename);
         }
-        Assertions.assertTrue(handler.toString().contains("bytes=" + Arrays.toString(content.getBytes())));
+        Assertions.assertTrue(handler.toString().contains(check));
+        Assertions.assertFalse(handler.toString().contains(check2));
     }
 
 }
