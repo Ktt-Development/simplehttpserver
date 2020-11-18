274f8cc74129b3f0021473b8a01779287f7300cc
Katsute <58778985+Katsute@users.noreply.github.com>
2020-11-17 21:20:44 -0500

    cleanup

--- src/main/java/com/kttdevelopment/simplehttpserver/HttpRequestMethod.java
+++ src/main/java/com/kttdevelopment/simplehttpserver/HttpRequestMethod.java
@@ -7,17 +7,18 @@ package com.kttdevelopment.simplehttpserver;
  * @version 4.3.0
  * @author Ktt Development
  */
+@SuppressWarnings("unused")
 public abstract class HttpRequestMethod {
 
-    public static final String GET = "GET";
-    public static final String HEAD = "HEAD";
-    public static final String POST = "POST";
-    public static final String PUT = "PUT";
-    public static final String DELETE = "DELETE";
-    public static final String CONNECT = "CONNECT";
-    public static final String OPTIONS = "OPTIONS";
-    public static final String TRACE = "TRACE";
-    public static final String PATCH = "PATCH";
-    public static final String UNSUPPORTED = "UNSUPPORTED";
+    public static final String GET          = "GET";
+    public static final String HEAD         = "HEAD";
+    public static final String POST         = "POST";
+    public static final String PUT          = "PUT";
+    public static final String DELETE       = "DELETE";
+    public static final String CONNECT      = "CONNECT";
+    public static final String OPTIONS      = "OPTIONS";
+    public static final String TRACE        = "TRACE";
+    public static final String PATCH        = "PATCH";
+    public static final String UNSUPPORTED  = "UNSUPPORTED";
 
 }
